4af12148e6d2a764b56f7847f6836053dc7a8a9e
Jens Lövgren <jens.lovgren@outlook.com>
2019-12-05 16:57:32 +0100

    Lagt till rättningsfunktion, kordinater till knappar, lagt till pusselinput

--- wwwroot/javascript/javascript.js
+++ wwwroot/javascript/javascript.js
@@ -1,17 +1,50 @@
-﻿let toggle = false;
+﻿var correctArray = [
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0],
+    [1, 1, 0, 1, 1],
+    [0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0]
+];
+var guessArray = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+];
 
-function buttonBlack(_this) {
 
-    if (!toggle) {
+function buttonBlack(_this, x, y) {
+
+    if (_this.style.backgroundColor != "black") {
         _this.style.backgroundColor = "black";
-        toggle = true;
+        guessArray[y][x]=1
     }
     else {
         _this.style.backgroundColor = "white";
-        toggle = false;
+        guessArray[y][x] = 0
     }
+
 }
 
-function createButton(){
+function check() {
+    var grattis = document.getElementById('congrats');
+
+    let counter = 0
+    for (var i = 0; i < correctArray.length; i++) {
+
+        for (var j = 0; j < correctArray.length; j++) {
+
+            if (correctArray[j][i] !== guessArray[j][i]) {
+                counter++
+            }
+        }
+    }
+
+    if (counter > 0) {
+        grattis.innerHTML = "FAN! "
+    }
+    else
+        grattis.innerHTML = "GRATTIS!"
 
 }
