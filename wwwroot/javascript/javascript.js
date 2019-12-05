3ea1903940f45c1507863131137140628f14d810
Daniel <andersson.daniiel@hotmail.com>
2019-12-05 15:31:59 +0100

    layout buttons

--- wwwroot/javascript/javascript.js
+++ wwwroot/javascript/javascript.js
@@ -1,3 +1,17 @@
-﻿function buttonBlack(_this) {
-    _this.style.backgroundColor = "black";
+﻿let toggle = false;
+
+function buttonBlack(_this) {
+
+    if (!toggle) {
+        _this.style.backgroundColor = "black";
+        toggle = true;
+    }
+    else {
+        _this.style.backgroundColor = "white";
+        toggle = false;
+    }
+}
+
+function createButton(){
+
 }
