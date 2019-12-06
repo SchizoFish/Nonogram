280bf06b5c3b9f879ad52994af19ce7ca83e7b7c
Jens Lövgren <jens.lovgren@outlook.com>
2019-12-06 13:19:24 +0100

    Startsida, återgå från check, sifforor ovanför, blider

--- wwwroot/javascript/javascript.js
+++ wwwroot/javascript/javascript.js
@@ -1,11 +1,11 @@
-﻿var correctArray = [
+﻿let correctArray = [
     [0, 0, 1, 0, 0],
     [0, 1, 1, 1, 0],
     [1, 1, 0, 1, 1],
     [0, 1, 1, 1, 0],
     [0, 0, 1, 0, 0]
 ];
-var guessArray = [
+let guessArray = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
@@ -18,19 +18,23 @@ function buttonBlack(_this, x, y) {
 
     if (_this.style.backgroundColor != "black") {
         _this.style.backgroundColor = "black";
-        guessArray[y][x]=1
+        guessArray[y][x] = 1
     }
     else {
         _this.style.backgroundColor = "white";
         guessArray[y][x] = 0
     }
 
+    var grattis = document.getElementById('congrats');
+    grattis.innerHTML = "DÖDSSVÅRT"
+
 }
 
 function check() {
+    let counter = 0
     var grattis = document.getElementById('congrats');
 
-    let counter = 0
+    
     for (var i = 0; i < correctArray.length; i++) {
 
         for (var j = 0; j < correctArray.length; j++) {
@@ -42,9 +46,15 @@ function check() {
     }
 
     if (counter > 0) {
-        grattis.innerHTML = "FAN! "
+        grattis.innerHTML = "FAN!  " + counter + "  FEL . . . ."
     }
-    else
+    else {
         grattis.innerHTML = "GRATTIS!"
+    }
+        
 
+}
+
+function uncheck() {
+   
 }
